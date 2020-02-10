638c5ea36d4704ac8d0f56a224d649aac505ced3
KangByoungWoo <koolkb@KangByoungWooui-MacBook-Pro-3.local>
2020-02-10 14:43:15 +0900

    modified test code

--- 2_1_Base_Volumes.js
+++ 2_1_Base_Volumes.js
@@ -1,57 +1,43 @@
 describe('OFManager Base Volume Test', function()  {
-  beforeEach('Visit OpenFrame Manager ', () => {
+  beforeEach('Visit OpenFrame Manager', function() {
     cy.visit('http://192.168.14.110:1122/ofmanager/#!/login')
 
-    cy.get('#input-id')
-      .type('ROOT').should('have.value', 'ROOT')
+    cy.get('#input-id').type('ROOT').should('have.value', 'ROOT')
+    cy.get('#input-password').type('SYS1').should('have.value', 'SYS1')
+    cy.get('#button-submit').click(2000)
 
-    cy.get('#input-password')
-      .type('SYS1').should('have.value', 'SYS1')
-
-    cy.get('#button-submit').click(1000)
+    cy.screenshot('2_1_base_volumes')
   })
 
-  it('Base > Volume : Volume list Check', () => {
+  it('Base > Volumes : Volume full list', () => {
+    cy.get('.top-menu_menu-lnb_volumes').click(3000)
     cy.screenshot('2_1_base_volumes')
+   })
 
-    cy.server()
-
-    cy.route({
-	method: 'POST',
-	url: '**/base/volumes',
-	response: [{
-        volume: "100001",
-        nonVsamUsage: "12.60(%)",
-        nonVsamTotal: "410(GB)",
-        vsamUsage: "0.25(%)",
-        vsamTotal: "100(MB)",
-        path: "/home/koolkb/oframe7/OHOME/volume_100001",
-	deviceType: "3380"}, {
-	}, {
-        volume: "100002",
-        nonVsamUsage: "12.60(%)",
-        nonVsamTotal: "410(GB)",
-        vsamUsage: "0.25(%)",
-        vsamTotal: "100(MB)",
-        path: "/home/koolkb/oframe7/OHOME/volume_100002",
-        deviceType: "3380"
-	}, {
-        volume: "DEFVOL",
-        nonVsamUsage: "12.60(%)",
-        nonVsamTotal: "410(GB)",
-        vsamUsage: "99.98(%)",
-        vsamTotal: "668(MB)",
-        path: "/home/koolkb/oframe7/OHOME/volume_DEFVOL",
-        deviceType: "3380"
-	}, {
-        volume: "VSPOOL",
-        nonVsamUsage: "12.60(%)",
-        nonVsamTotal: "410(GB)",
-        vsamUsage: "",
-        vsamTotal: "",
-        path: "/home/koolkb/oframe7/OHOME/spool",
-        deviceType: ""
-	}]
-	})
+  it('Base > Volumes : Volume search', () => {
+    cy.get('.top-menu_menu-lnb_volumes').click(3000)
+    cy.get('#textfield-volume').find('.top-textfield-text').type('DEFVOL').should('have.value', 'DEFVOL')
+    cy.get('#Button141_1').find('.top-button-root').click(1000)
+    cy.request('POST', 'http://192.168.14.110:1122/ofmanager/batch/get-volumes-list-paging',
+        {volume: "DEFVOL",
+          devicetype: "",
+          path: "",
+          pageLength: "20",
+          currentPage: "0",
+          startIndex: 0,
+          sortOrderBy: "1",
+          sortColumnName: "Volume Serial"
+        }).then((response) => {
+        (expect(response.body.list[0]).to.deep.equal({
+            volume: "DEFVOL",
+            nonVsamUsage: "12.60(%)",
+            nonVsamTotal: "410(GB)",
+            vsamUsage: "99.98(%)",
+            vsamTotal: "668(MB)",
+            path: "/home/koolkb/oframe7/OHOME/volume_DEFVOL",
+            deviceType: "3380"
+      }))
+    })
+    cy.screenshot('2_1_base_volumes')
   })
 })
--- 2_2_Base_Catalogs.js
+++ 2_2_Base_Catalogs.js
@@ -9,6 +9,8 @@ describe('OFManager Base Catalog Test', function()  {
       .type('SYS1').should('have.value', 'SYS1')
 
     cy.get('#button-submit').click(1000)
+  
+    cy.screenshot('2_2_base_catalogs')
   })
 
   it('Base > Catalog : Catalog list Check', () => {
@@ -17,7 +19,13 @@ describe('OFManager Base Catalog Test', function()  {
     cy.get('.top-menu_menu-lnb_catalogs').click(1000)
 
     cy.request('POST', 'http://192.168.14.110:1122/ofmanager/base/get-catalogs-list',
-        {catalogName: "",  volumeName: "", pageLength: "20", currentPage: "0", startIndex: 0, sortOrderBy: "1", sortColumnName: "Catalog Name"
+        {catalogName: "",  
+     volumeName: "",  
+     pageLength: "20", 
+     currentPage: "0", 
+     startIndex: 0,  
+     sortOrderBy: "1", 
+     sortColumnName: "Catalog Name"
         }).then((response) => {
         (expect(response.body.list[0]).to.deep.equal({
            name: "SYS1.MASTER.ICFCAT",
@@ -26,5 +34,26 @@ describe('OFManager Base Catalog Test', function()  {
            supname:""
         }))
       })
+    })
+  
+  it('Base > Catalog : fixture Test', () => {
+    cy.screenshot('2_2_base_catalogs')
+
+    cy.get('.top-menu_menu-lnb_catalogs').click(1000)
+
+    cy.request('POST', 'http://192.168.14.110:1122/ofmanager/base/get-catalogs-list',
+        {catalogName: "",  
+     volumeName: "",  
+     pageLength: "20", 
+     currentPage: "0", 
+     startIndex: 0,  
+     sortOrderBy: "1", 
+     sortColumnName: "Catalog Name"
+        }).then((response) => {
+      cy.writeFile('cypress/fixtures/catalog_item.json', response.body.list)
+    })
+  cy.fixture('catalog_item').should((catalog_item) => {
+    expect(catalog_item[0].name).to.eq('SYS1.MASTER.ICFCAT')
+     })    
     })
 })
